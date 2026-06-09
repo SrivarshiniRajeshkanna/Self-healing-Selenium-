bb650d890d24eba5e6c2b83e6a432fd22f959772
SrivarshiniRajeshkanna <varshudpm09@gmail.com>
2026-06-09 18:19:57 +0530

    deployment code

--- core/driver_factory.py
+++ core/driver_factory.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+import subprocess
 
 class DriverFactory:
 
@@ -12,8 +13,13 @@ class DriverFactory:
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920,1080")
-        options.binary_location = "/usr/bin/chromium"  # Render's Chrome path
 
-        service = Service("/usr/bin/chromedriver")  # Render's ChromeDriver path
+        # Auto-detect chromium path on Render
+        chrome_path = subprocess.check_output(["which", "chromium"]).decode().strip()
+        driver_path = subprocess.check_output(["which", "chromedriver"]).decode().strip()
+
+        options.binary_location = chrome_path
+        service = Service(driver_path)
+
         driver = webdriver.Chrome(service=service, options=options)
         return driver
